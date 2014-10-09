84f8ef5e390dee996ab505ae73ad3e0515ab829d
dario-c <dariocavanillas@gmail.com>
2014-10-09 17:16:16 +0200

    Completed mouse event functionality

--- js/raum.js
+++ js/raum.js
@@ -1,13 +1,25 @@
 
-$('main').on("mouseenter", 'section', function() {
+$(document).on("click", function(){
+  $('.information').addClass('hidden');
+});
+
+$('main').on("mouseenter", 'section', function(event) {
+
+  event.stopPropagation();
+  $('main').find('h1').removeClass('selected');
+  $(this).find('h1').addClass('selected');
 
   $('aside').find('section').addClass('hidden');
   var associated = $(this).data('punkte')+'-punkte';
   $('#'+associated).removeClass('hidden');
 });
 
-$('h1').on("click", function(){
+
+
+$('main').on("click", 'section', function(event){
+  event.stopPropagation();
   $('.information').addClass('hidden');
   $(this).closest('section').find('div').removeClass('hidden');
 
 });
+
