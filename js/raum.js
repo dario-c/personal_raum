5c337cdd3393f5b7e4aaa1e78c0e6d039b2e3c2f
dario-c <dariocavanillas@gmail.com>
2014-10-09 16:06:42 +0200

    Basic functionality of info p is finished

--- js/raum.js
+++ js/raum.js
@@ -1,13 +1,13 @@
 
-$('h1').on("mouseenter", function() {
+$('main').on("mouseenter", 'section', function() {
 
   $('aside').find('section').addClass('hidden');
-  var associated = $(this).closest('section').data('punkte')+'-punkte';
+  var associated = $(this).data('punkte')+'-punkte';
   $('#'+associated).removeClass('hidden');
 });
 
 $('h1').on("click", function(){
-
+  $('.information').addClass('hidden');
   $(this).closest('section').find('div').removeClass('hidden');
 
 });
