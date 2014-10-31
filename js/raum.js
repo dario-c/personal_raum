0b9430f998a138c3386ac2f721582ebcf96a4a72
dario-c <dariocavanillas@gmail.com>
2014-10-31 13:30:38 +0100

    Refactor js new Nav

--- js/raum.js
+++ js/raum.js
@@ -32,27 +32,27 @@ $('.pointExplanation').addClass('hidden');
 
 $("nav").on("mouseenter","div", function(){
     var image = $(this).closest("nav").find("img");
-    var file = "../PersonalRaum/images/";
-    
+    var filePath = "../PersonalRaum/images/";
+
   switch($(this)[0].className){
     case "ueber-uns":
-    image.attr("src",file+"menu_about_selected.png");
+    image.attr("src",filePath+"menu_about_selected.png");
     break;
     case "services":
-    image.attr("src",file+"menu_services_selected.png");
+    image.attr("src",filePath+"menu_services_selected.png");
     break;
     case "team":
-    image.attr("src",file+"menu_team_selected.png");
+    image.attr("src",filePath+"menu_team_selected.png");
     break;
     case "contact":
-    image.attr("src",file+"menu_contact_selected.png");
+    image.attr("src",filePath+"menu_contact_selected.png");
     break;
     default:
-    image.attr("src",file+"menu_none_selected.png");
+    image.attr("src",filePath+"menu_none_selected.png");
     break;
   }
 });
 
 $("main").on("mouseleave", function(){
-  $(this).find("img").attr("src","file:///home/dario/Code/PersonalRaum/textPersonalRaum/141023_FILES-WEBSITE%202/141023_FILES-WEBSITE/none_selected.png");
+  $(this).find("img").attr("src",filePath+"none_selected.png");
 });
